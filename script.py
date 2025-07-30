b36dc6cb25e832f48b0ee5a7774944253fff18a0
alishaf18 <alishafaraz@gmail.com>
2025-07-30 10:11:05 -0400

    second part

--- script.py
+++ script.py
@@ -16,3 +16,24 @@ def main():
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, 
                                             shuffle=True, num_workers=4) # Shuffles elements before each batch, how many workers to work on unpacking info
+
+     print("Loading test dataset...")
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, # FALSE means this is a test dataset
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
+    print("Defining class labels...")
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck') # Classes to be labled
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    print("Defining helper function to display images...")
+    # functions to show an image
+    def imshow(img):
+        img = img / 2 + 0.5 # normalizing the pixel range 
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0))) # Rearranging the axis of the original np image
+        plt.show() # Showing the lot
